cacf1943361a0a5aaa4675490788b97ab996e752
Tran Van Duy <duytv@ait.com>
2022-07-29 16:09:59 +0700

    ok

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('data.json');
+const router = jsonServer.router('./temp/data.json');
 const middlewares = jsonServer.defaults({
     static: './build'
 })
