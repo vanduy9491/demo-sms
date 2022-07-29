3f859e18ad4ebe0f6d088fe55dd35e67770d6bc7
vanduy9491 <81944197+vanduy9491@users.noreply.github.com>
2022-07-29 15:25:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const middlewares = jsonServer.defaults({
     static: './build'
 })
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 server.use(middlewares);
 server.use(
     jsonServer.rewriter({
